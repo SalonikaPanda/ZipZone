e2530fdfae7c67ae26e7c9cf947ecbbdd35ab6d9
SalonikaPanda <134544828+SalonikaPanda@users.noreply.github.com>
2025-05-20 22:32:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const zip = document.getElementById("zip");
+let ResultContainer = document.getElementById("SearchResult");
+async function getArea() {
+    const zipInput = zip.value;
+    const url = `https://api.zippopotam.us/in/${zipInput}`;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error("Invalid ZIP Code or not found!");
+        }
+        const data = await response.json();
+        const pincode = data["post code"];
+        ResultContainer.innerHTML = '';
+        const place = data.places;
+
+        place.forEach(place => {
+            const placeResult = place["place name"];
+            const stateResult = place["state"];
+            const ResultBox = document.createElement("div");
+            ResultBox.className = "resultBox";
+            ResultBox.innerHTML = `<p>${placeResult}</p>
+            <p>${stateResult},${pincode}</p>`;
+            ResultContainer.appendChild(ResultBox);
+        });
+
+    } catch (error) {
+    ResultContainer.classList.add("errorBox");
+        ResultContainer.innerHTML = `Error: ${error.message}`;
+    }
+}
